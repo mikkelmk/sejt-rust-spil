946b43c3e1fa0060cc8c37d3eab0f1f755e8e3e1
mikkelmk <mikkelmk@outlook.com>
2024-05-25 17:17:49 +0200

    technically v1 of the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("");
+        println!("You are in a game, what do you want to do?");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input = input.trim();
+
+        if input == "win the game" {
+            let random_number = rand::thread_rng().gen_range(1..=3);
+            if random_number == 1 {
+                println!("You won the game!");
+                break;
+            }
+            println!("Didn't work, try again!");
+        } else {
+            println!("You can't do that!");
+        }
+    }
 }
